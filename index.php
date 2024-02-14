98b09c9043f82614a4398f009c126861d9be4d20
nguyenluongson06 <nguyenluongson06@gmail.com>
2024-02-14 21:48:04 +0700

    add entry page with placeholders
    TODO: finish other pages

--- index.php
+++ index.php
@@ -10,6 +10,112 @@
     <!-- common header -->
     <?php include("components/header.html") ?>
 
+    <!-- main content section -->
+
+    <section class="section-content">
+
+            <div class="welcome-cover" style="background-image: url('images/slider.jpg');">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-6 col-sm-offset-6 col-md-5 col-md-offset-7">
+                            
+                            <h2>Welcome to the Katharine</h2>
+                            <h4>Trendy Fashion Blog</h4>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="container pv14 welcome-content">
+                <div class="row">
+                    <div class="col-sm-6 col-md-4">
+                        <div class="welcome-item">
+                            <a href="home-1.html">
+                                <img src="images/thumbnail/1.jpg" alt="welcome item">
+                            </a>
+                            <h3>FASHION BLOG 01</h3>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4">
+                        <div class="welcome-item">
+                            <a href="home-2.html">
+                                <img src="images/thumbnail/2.jpg" alt="welcome item">
+                            </a>
+                            <h3>FASHION BLOG 02</h3>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4">
+                        <div class="welcome-item">
+                            <a href="home-3.html">
+                                <img src="images/thumbnail/3.jpg" alt="welcome item">
+                            </a>
+                            <h3>FASHION BLOG 03</h3>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4">
+                        <div class="welcome-item">
+                            <a href="home-4.html">
+                                <img src="images/thumbnail/4.jpg" alt="welcome item">
+                            </a>
+                            <h3>FASHION BLOG 04</h3>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4">
+                        <div class="welcome-item">
+                            <a href="home-5.html">
+                                <img src="images/thumbnail/5.jpg" alt="welcome item">
+                            </a>
+                            <h3>FASHION BLOG 05</h3>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4">
+                        <div class="welcome-item">
+                            <a href="home-6.html">
+                                <img src="images/thumbnail/6.jpg" alt="welcome item">
+                            </a>
+                            <h3>FASHION BLOG 06</h3>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4">
+                        <div class="welcome-item">
+                            <a href="home-7.html">
+                                <img src="images/thumbnail/7.jpg" alt="welcome item">
+                            </a>
+                            <h3>FASHION BLOG 07</h3>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4">
+                        <div class="welcome-item">
+                            <a href="home-8.html">
+                                <img src="images/thumbnail/8.jpg" alt="welcome item">
+                            </a>
+                            <h3>FASHION BLOG 08</h3>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4">
+                        <div class="welcome-item">
+                            <a href="home-9.html">
+                                <img src="images/thumbnail/9.jpg" alt="welcome item">
+                            </a>
+                            <h3>FASHION BLOG 09</h3>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-md-4"></div>
+                    <div class="col-sm-6 col-md-4">
+                        <div class="welcome-item">
+                            <a href="home-10.html">
+                                <img src="images/thumbnail/10.jpg" alt="welcome item">
+                            </a>
+                            <h3>FASHION BLOG 10</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
     <!-- common footer -->
     <?php include("components/footer.html") ?>
 

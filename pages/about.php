82d2424a4924c9b13be5a1bc6d1841cd47210b74
nguyenluongson06 <nguyenluongson06@gmail.com>
2024-02-15 15:25:41 +0700

    change path used to absolute

--- pages/about.php
+++ pages/about.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Us</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
-    <?php include("components/header.html") ?>
+    <?php include("../components/header.html") ?>
 
-    <?php include("components/footer.html") ?>
+    <?php include("../components/footer.html") ?>
 </body>
 </html>

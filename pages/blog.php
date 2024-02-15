8bcf701c6d664d0a85cedc86a3a824cd36bf7061
nguyen viet thanh <153370774+thanhnvkkl@users.noreply.github.com>
2024-02-15 14:58:28 +0700

    l

--- pages/blog.php
+++ pages/blog.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Katharine Blog</title>
-    <link rel="stylesheet" href="css/blog.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 
@@ -13,22 +13,22 @@
     <section>
         <article>
             <img src="images/blogimg/b1.jpg" alt="Ảnh đồ xuân" class="featured-image">
-            <h2>Quần Áo Mùa Xuân</h2>
+            <h3>Quần Áo Mùa Xuân</h3>
             <p>Quần áo xuân là bộ sưu tập đặc trưng được thiết kế để phản ánh tinh thần của mùa xuân. Trong thời trang, mỗi mùa đều mang đến một loạt các xu hướng và phong cách riêng biệt, và quần áo xuân không phải là ngoại lệ,quần áo xuân không chỉ là về phong cách mà còn là về cách bạn cảm nhận về mùa xuân và cảm xúc mà nó đem lại. Hãy tự do để thể hiện phong cách và cá nhân của bạn thông qua bộ sưu tập quần áo xuân của mình!</p>
         </article>
         <article>
             <img src="images/blogimg/b2.jpg" alt="Ảnh đồ hè" class="featured-image">
-            <h2>Quần Áo Mùa Hè</h2>
+            <h3>Quần Áo Mùa Hè</h3>
             <p>Quần áo mùa hè được thiết kế để mang lại sự thoải mái và mát mẻ trong những ngày nắng nóng của mùa hè, quần áo mùa hè không chỉ là về phong cách mà còn là về sự thoải mái và bảo vệ sức khỏe. Hãy chọn những bộ trang phục phản ánh phong cách của bạn và giúp bạn cảm thấy thoải mái nhất trong những ngày nắng nóng của mùa hè.</p>
         </article>
         <article>
             <img src="images/blogimg/b3.jpg" alt="Ảnh đồ đông" class="featured-image">
-            <h2>Quần Áo Mùa Đông</h2>
+            <h3>Quần Áo Mùa Đông</h3>
             <p>Quần áo mùa đông được thiết kế để giữ ấm và bảo vệ cơ thể khỏi thời tiết lạnh giá của mùa đông, quần áo mùa đông không chỉ là về việc giữ ấm mà còn là về phong cách và sự bảo vệ sức khỏe. Hãy chọn những bộ trang phục phản ánh phong cách của bạn và giúp bạn cảm thấy ấm áp và thoải mái trong mùa đông.</p>
         </article>
         <article>
             <img src="images/blogimg/b4.jpg" alt="Ảnh đồ thu" class="featured-image">
-            <h2>Quần Áo Mùa Đông</h2>
+            <h3>Quần Áo Mùa Đông</h3>
             <p>Quần áo mùa thu thường được thiết kế để chống chọi với sự chuyển đổi từ thời tiết ấm áp của mùa hè sang những ngày se lạnh của mùa đông, quần áo mùa thu không chỉ là về việc chuẩn bị cho thời tiết mát mẻ mà còn là về phong cách và thoải mái.</p>
         </article>
     </section>
